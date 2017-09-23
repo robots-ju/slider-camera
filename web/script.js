7706e4a2917aa94379f445343fcf4f0660a99895
bluedrack28 <bluedrack28@gmail.com>
2017-09-23 16:47:06 +0200

    Ajout de commentaire dans script.js

--- web/script.js
+++ web/script.js
@@ -1,7 +1,7 @@
+//Declartions de toutes les variable principales
 const canvas = document.getElementById('canvas')
 let pos1 = document.getElementById('position1')
 let pos2 = document.getElementById('position2')
-
 const ctx = canvas.getContext('2d')
 let tablePoint = []
 let maxPoint = 2
@@ -9,32 +9,19 @@ let selectedPoint = null;
 
 //Ajout de tout les evenement des boutons
 
-function addPoint(){
-  if(tablePoint.length+1 <= maxPoint){
-    let identity = tablePoint.length;
-    tablePoint[tablePoint.length] = {
-        selected: false,
-        id: identity + 1,
-        x: tablePoint.length*20,
-        y: 0,
-        height: 20,
-        width: 20
-    }
-  }else{
-    console.log('nbr max de point atteint');
-  }
-}
-
 document.getElementById('start').addEventListener('click',(e)=>{
   console.log('salut')
 });
+
 document.getElementById('clean').addEventListener('click',(e)=>{
   tablePoint = []
   cleanCtx()
 });
+
 document.getElementById('try').addEventListener('click',(e)=>{
   simuled()
 })
+
 document.getElementById('stop').addEventListener('click',(e)=>{
   console.log('salut')
 });
@@ -42,8 +29,9 @@ document.getElementById('stop').addEventListener('click',(e)=>{
 document.getElementById('add').addEventListener('click',eventAddPoint);
 
 document.getElementById('delete').addEventListener('click',(e)=>{
-
 });
+
+// Gestion des evenements sur le canvas
 
 canvas.addEventListener('click',(e)=>{
   let pos = getMousePos(canvas,e)
@@ -64,6 +52,26 @@ canvas.addEventListener('click',(e)=>{
   }
 });
 
+// Déclarations des functions principales
+
+function addPoint(){
+  if(tablePoint.length+1 <= maxPoint){
+    let identity = tablePoint.length;
+    tablePoint[tablePoint.length] = {
+        selected: false,
+        id: identity + 1,
+        x: tablePoint.length*20,
+        y: 0,
+        height: 20,
+        width: 20
+    }
+  }else{
+    console.log('nbr max de point atteint');
+  }
+}
+
+// Revoit la valeur relative au canvas du curseur de la souris
+
 function getMousePos(canvas, evt) {
     var rect = canvas.getBoundingClientRect();
     return {
@@ -71,6 +79,9 @@ function getMousePos(canvas, evt) {
       y: evt.clientY - rect.top
     };
 }
+
+// Vérifie si le point est dans un carré défini
+
 function isIn(position,scare){
   try {
     if(scare.x < position.x &&
@@ -85,14 +96,22 @@ function isIn(position,scare){
     return false
   }
 }
+
+// Inscrit la position d'un point par rapport à la position désiré
+
 function setPointPosition(point,position){
   point.x = position
 }
+
+// Fonction du bouton "Ajouter un bouton"
 
 function eventAddPoint(){
   addPoint()
   update()
 }
+
+// Draw un point dans le canvas
+
 function drawPoint(point){
   if(point.selected == true){
     ctx.fillStyle='#009933'
@@ -104,6 +123,8 @@ function drawPoint(point){
   ctx.font='20px DejaVu Sans Mono'
   ctx.fillText(point.id,point.x,20)
 }
+
+// Update la page web
 
 function update(){
   cleanCtx()
@@ -119,6 +140,9 @@ function update(){
     drawPoint(tablePoint[i])
   }
 }
+
+// Fonction de simulation de la trajectoire 
+
 function simuled(){
   let x1 = tablePoint[0].x
   let x2 = tablePoint[1].x
@@ -136,33 +160,28 @@ function simuled(){
       x1++;
     }else{
       if(x1<x2) break;
-      x2++;
+      x1--;
     }
-    
   }
+}
 
-}
-/*
-  si direction est vrai il avance si elle est fausse elle recule
-*/
-function draw(x1,x2,direction){
-  ctx.fillStyle='#009933'
-  ctx.fillRect(x1,0,20,20);
-  
-  if(direction){
-    if(x1>x2);
-    x1++;
-    
-  }else{
-    if(x1<x2);
-    x1--;
-  }
-  requestAnimationFrame(draw(x1,x2,direction))
-}
+// Clean le canvas à son état initale
+
 function cleanCtx(){
   ctx.fillStyle='#FFFFFF'
   ctx.fillRect(0,0,canvas.width,canvas.height)
   ctx.fillStyle='#000000'
   ctx.fillRect(0,10,canvas.width,5)
 }
-cleanCtx()
+
+// Renvoit la position des points du tableau "tablePoint"
+
+function getPosition(){
+  try {
+    return [tablePoint[0],tablePoint[1]]
+  } catch (error) {
+    console.log('les points net sont pas bien definit !');
+  } 
+}
+
+cleanCtx();
